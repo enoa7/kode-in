402928daf1f174facd5dcb1218027670565211c7
enoa7 <gamal.ardi@gmail.com>
2016-02-10 23:23:54 +0700

    add kodein logo, add modernizrjs for svg

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php require('function.php'); ?>
 <?php get_header(); ?>
-test
+<!-- <div class="site-navigation">
+	this is navigation
+</div> -->
+<div class="site-content">
+	<div class="container">
+		<div class="site-logo">
+			
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
