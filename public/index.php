95408c63cdbdd4f209dcde74900b563fb817042a
enoa7 <gamal.ardi@gmail.com>
2016-02-12 07:15:41 +0700

    update css files and structure

--- public/index.php
+++ public/index.php
@@ -3,14 +3,23 @@
 <!-- <div class="site-navigation">
 	this is navigation
 </div> -->
-<div class="site-content">
+<div class="site-page underconstruction">
 	<div class="container">
 		<div class="site-logo">
-			<svg class="">
-		        <use xlink:href="../img/kodein-logo.svg#kodein-logo"></use>
-		    </svg>
-		    <?php //echo file_get_contents('../img/kodein-logo.svg#kodein-logo'); ?>
+		    <?php echo file_get_contents('../img/kodein-logo.svg'); ?>
 		</div>
+		<div class="site-content">
+			<div class="custom-container">
+				<h1 class="title">Website Under Construction.</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="site-footer">
+	<div class="container">
+		<div class="copyright">
+			Copyright Kode-In 2016
+		</div>	
 	</div>
 </div>
 <?php get_footer(); ?>
